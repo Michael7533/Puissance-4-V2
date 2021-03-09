d86b6e8b6090b2d4ccbc86c930a7e8714fcadb7d
Michael <eoloneox@gmail.com>
2021-03-09 14:00:44 +0100

    ajout des variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Liste des variables
+        int nbl = 8 ;//nombre de lignes dans le tableau
+        int nbc = 9 ; // nombre de colonnes dans le tableau
+        int ruleCoins = 4 ; // nombre de jetons alignés pour victoire
+        String player1 ; // nom du joueur 1
+        String player2 ; // nom du joueur 2
+        char currentPlayer; // joueur dont le tour est en cours
+        boolean win; // True pour victoire, false pour défaite
+        int SumResult=0;//Nombre de jetons alignés
+        char[][] board = new char[nbl][nbc]; // tableau du puissance 4
+    }
+}
